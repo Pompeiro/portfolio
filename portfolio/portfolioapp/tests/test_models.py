a60adf8c34a4c931c255f0bdd42b4d0b42ee0c2f
Pompeiro <pompeiro10@wp.pl>
2021-06-19 15:49:42 +0200

    Use PortfolioProjectFactory in a test

--- portfolio/portfolioapp/tests/test_models.py
+++ portfolio/portfolioapp/tests/test_models.py
@@ -1,18 +1,11 @@
 import pytest
 
-from ..models import PortfolioProject
+from .factories import PortfolioProjectFactory
 
 pytestmark = pytest.mark.django_db
 
 
 def test___str__():
-    project = PortfolioProject.objects.create(
-        title="Mega super usefull project that will shine in my portfolio",
-        description="The best project ever seen. You couldn't even imagine someone would invent this.",
-        website="https://www.github.com/",
-    )
-    assert (
-        project.__str__()
-        == "Mega super usefull project that will shine in my portfolio"
-    )
-    assert str(project) == "Mega super usefull project that will shine in my portfolio"
+    project = PortfolioProjectFactory()
+    assert project.__str__() == project.title
+    assert str(project) == project.title

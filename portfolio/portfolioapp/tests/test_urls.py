ba89d07ddf06402d043eeb01e39fcc63085437dc
Pompeiro <pompeiro10@wp.pl>
2021-06-20 09:10:57 +0200

    Write tests for all portfolioapp URL patterns

---
+++ portfolio/portfolioapp/tests/test_urls.py
@@ -0,0 +1,37 @@
+import pytest
+from django.urls import resolve, reverse
+
+from .factories import PortfolioProjectFactory
+
+pytestmark = pytest.mark.django_db
+
+
+@pytest.fixture
+def project():
+    return PortfolioProjectFactory()
+
+
+def test_list_reverse():
+    assert reverse("portfolioapp:list") == "/portfolioapp/"
+
+
+def test_list_resolve():
+    assert resolve("/portfolioapp/").view_name == "portfolioapp:list"
+
+
+def test_add_reverse():
+    assert reverse("portfolioapp:add") == "/portfolioapp/add/"
+
+
+def test_add_resolve():
+    assert resolve("/portfolioapp/add/").view_name == "portfolioapp:add"
+
+
+def test_detail_reverse(project):
+    url = reverse("portfolioapp:detail", kwargs={"slug": project.slug})
+    assert url == f"/portfolioapp/{project.slug}/"
+
+
+def test_detail_resolve(project):
+    url = f"/portfolioapp/{project.slug}/"
+    assert resolve(url).view_name == "portfolioapp:detail"

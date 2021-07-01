7c129eb7e067bb8fd0d34f64700d1b9271512ae7
Pompeiro <pompeiro10@wp.pl>
2021-07-01 12:53:53 +0200

    Add Technology model

--- portfolio/portfolioapp/admin.py
+++ portfolio/portfolioapp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import PortfolioProject
+from .models import PortfolioProject, Technology
 
 admin.site.register(PortfolioProject)
+admin.site.register(Technology)
--- portfolio/portfolioapp/models.py
+++ portfolio/portfolioapp/models.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 
 
 class PortfolioProject(models.Model):
-    title = models.CharField("Title of project", max_length=100)
+    title = models.CharField("Title of project", max_length=100, unique=True)
     description = models.TextField("Description", blank=True)
     website = models.URLField(blank=True)
     try_online_url = models.URLField(blank=True)
@@ -21,3 +21,13 @@ class PortfolioProject(models.Model):
     def get_absolute_url(self):
         """Return absolute URL to the PortfolioProject Detail page."""
         return reverse("portfolioapp:detail", kwargs={"slug": self.slug})
+
+
+class Technology(models.Model):
+    technology = models.CharField(max_length=200)
+    project = models.ForeignKey(
+        PortfolioProject, on_delete=models.CASCADE, to_field="title"
+    )
+
+    def __str__(self):
+        return self.technology
